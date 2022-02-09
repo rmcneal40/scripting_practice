a1dfeee8d11867bb68e6093be3c3059256d2cf18
Robert McNeal <mcneal.robert40@yahoo.com>
2022-02-08 21:17:12 -0600

    Practice runs with Python, creating objects and the python basics

---
+++ objectoriented.py
@@ -0,0 +1,26 @@
+
+
+
+import re
+
+
+class Ops:
+   def __init__(self, name, year):
+       self.name = name
+       self.year = year
+p1 = Ops("CyberSecurity", 2022)
+print(p1.year)
+print(p1.name)
+
+class Technology:
+    def __init__(self, name, year, grade, gpa, technology):
+        self.name = name
+        self.year = year
+        self.grade = grade
+        self.gpa = gpa
+        self.technology = technology
+    def myfunc(records):
+      print("Hello my grade is " + records.grade + "this is " + records.year + "my grade is " + records.grade 
+      + "I have a high " + records.gpa + "Im in the school of " + records.technology)
+p1 = Technology("A", "2022", "D", "3.0", "cyber")
+p1.myfunc()
