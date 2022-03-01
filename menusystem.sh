e71ca847c201537746cdfb3f0d3e878801318d7f
Robert McNeal <mcneal.robert40@yahoo.com>
2022-02-28 19:54:36 -0600

    This is a bash script that launches a menu system

---
+++ menusystem.sh
@@ -0,0 +1,24 @@
+# Create a bash script that launches a menu system with the following options:
+# Hello world (prints “Hello world!” to the screen)
+# Ping self (pings this computer’s loopback address)
+# IP info (print the network adapter information for this computer)
+# Exit
+# Next, the user input should be requested.
+# The program should next use a conditional statement to evaluate the user’s input, then act according to what the user selected.
+# Use a loop to bring up the menu again after the request has been executed.
+
+#!/bin/bash
+
+
+echo "Hello world"
+read UserInput
+
+echo "pings this computer's loopback address"
+print the network adapter information for this computer
+
+function menu() {
+    do
+        echo "The program should next use a coditional statement"
+
+}
+menu
