2eeeb73428811c4b7e75f8a6e86c67db03814274
Robert McNeal <mcneal.robert40@yahoo.com>
2022-03-08 19:59:42 -0600

    Variable list

---
+++ variable.py
@@ -0,0 +1,17 @@
+# Create a Python script that includes the following:
+
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+
+from tkinter import Variable
+
+ list("fox" "cat" "dog" "bird" "owl" "rat" "bee") {
+
+}>>> print(imaginary_thing)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'imaginary_thing' is not defined
+>>>
